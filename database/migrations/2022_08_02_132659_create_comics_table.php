891a29c9ae921d65f2bee265c82036fe66b25ee5
bronzo97 <danilobronzo97@hotmail.it>
2022-08-02 15:52:57 +0200

    created table, migration and controller

---
+++ database/migrations/2022_08_02_132659_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longtext('description');
+            $table->float('price', 5, 2);
+            $table->string('series');
+            $table->string('type');
+            $table->date('sale_date', 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
