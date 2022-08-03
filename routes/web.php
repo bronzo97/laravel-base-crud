da9ca3cf90cbd344bfd90e75b8c25b856dfd1a93
bronzo97 <danilobronzo97@hotmail.it>
2022-08-03 16:05:40 +0200

    updates

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get("/", "ComicController@index")->name("comics.index");
-Route::get("/comics/{comic}", "ComicController@show")->name("comics.show");
+Route::get("/comics/show/{comic}", "ComicController@show")->name("comics.show");
 Route::get("/comics/create", "ComicController@create")->name("comics.create");
 
 Route::post("/comics", "ComicController@store")->name("comics.store");
